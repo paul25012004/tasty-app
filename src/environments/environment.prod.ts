fdcf0598a986391881bdbedfc75caec13411df34
@paul25012004 <paulkoumadi1@gmail.com>
2026-02-12 12:45:25 +0000

    Chore: Configure production environment and Netlify redirects

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,6 @@
+export const environment = {
+    production: true,
+    rapidApiKey: '4f986e135amsh479ca20409c1d16p1d80fajsn85a15e4809d5',
+    rapidApiHost: 'tasty.p.rapidapi.com',
+    apiUrl: 'https://tasty.p.rapidapi.com'
+};
